212a0bdf811e1c7a327c37ae0499b06d46042eb9
Bogdan Patseev <patseev1987@gmail.com>
2024-04-06 12:23:34 +0300

    add homework3 without h2

--- homework3/src/main/java/com/example/homework3/controllers/TaskController.java
+++ homework3/src/main/java/com/example/homework3/controllers/TaskController.java
@@ -2,8 +2,10 @@ package com.example.homework3.controllers;
 
 import com.example.homework3.domain.User;
 import com.example.homework3.services.DataProcessingService;
+import com.example.homework3.services.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +18,7 @@ import java.util.List;
 public class TaskController {
 
     @Autowired
-    private DataProcessingService service;
+    private RegistrationService service;
 
     @GetMapping
     public List<String> getAllTasks()
@@ -28,15 +30,30 @@ public class TaskController {
         return  tasks;
     }
 
-    @GetMapping("/sort")//localhost:8080/tasks/sort
+    @GetMapping("/sort")//localhost:1337/tasks/sort
     public List<User> sortUsersByAge()
     {
-        return service.sortUsersByAge(service.getRepository().getUsers());
+        return service.getDataProcessingService().sortUsersByAge(
+                service.getDataProcessingService().getRepository().getUsers()
+        );
     }
 
     //метод filterUsersByAge
-    //Подсказка  @GetMapping("/filter/{age}")
+    //Подсказка  @GetMapping("/filter/{age}") +
+    @GetMapping("/filter/{age}")
+    public List<User> filterUsersByAge(@PathVariable int age){
+        return service.getDataProcessingService().filterUsersByAge(
+                service.getDataProcessingService().getRepository().getUsers(),
+                age
+        );
+    }
 
     //метод calculateAverageAge
-    //Подсказка  @GetMapping("/calc")
+    //Подсказка  @GetMapping("/calc") +
+    @GetMapping("/calc")
+    public Double calcUsersByAge(){
+        return service.getDataProcessingService().calculateAverageAge(
+                service.getDataProcessingService().getRepository().getUsers()
+        );
+    }
 }
--- homework3/src/main/java/com/example/homework3/services/RegistrationService.java
+++ homework3/src/main/java/com/example/homework3/services/RegistrationService.java
@@ -6,14 +6,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class RegistrationService {
 
+
+    @Autowired
+    private DataProcessingService dataProcessingService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private NotificationService notificationService;
+
     public DataProcessingService getDataProcessingService() {
         return dataProcessingService;
     }
 
-    @Autowired
-    private DataProcessingService dataProcessingService;
 
-    //Поля UserService, NotificationService
+    //Поля UserService, NotificationService ++
 
-    //Метод processRegistration
+    //Метод processRegistration +
+
+    public void processRegistration(String name, int age, String email){
+        var user = userService.createUser(name, age, email);
+        dataProcessingService.addUserToList(user);
+        var msg = String.format("User\nname = %s\nage = %d\nemail = %s", user.getName(), user.getAge(), user.getEmail());
+        notificationService.sendNotification(msg);
+    }
+
+
 }
