b6c4ed9fb90b8d942b78c68692721a4cd1dac22b
Bogdan Patseev <patseev1987@gmail.com>
2024-05-09 23:06:43 +0300

    add microservice 1

---
+++ homework9/my-microserver-workers/src/test/java/ru/homeworks/mymicroserverworkers/MyMicroserverWorkersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.homeworks.mymicroserverworkers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMicroserverWorkersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
