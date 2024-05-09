b1f9eff176e931d6d1548b629fd1c9bc112d7f7d
Bogdan Patseev <patseev1987@gmail.com>
2024-05-09 23:01:38 +0300

    add eureka server

---
+++ homework9/my-microservice-tools/src/main/java/ru/homeworks/mymicroservicetools/MyMicroserviceToolsApplication.java
@@ -0,0 +1,13 @@
+package ru.homeworks.mymicroservicetools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMicroserviceToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMicroserviceToolsApplication.class, args);
+	}
+
+}
---
+++ homework9/my-microservice-tools/src/test/java/ru/homeworks/mymicroservicetools/MyMicroserviceToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.homeworks.mymicroservicetools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMicroserviceToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
